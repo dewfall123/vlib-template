fc9f79eaa62454fe43e4580ad9d4d86a6da90f29
zuofenghua <11100776@bbktel.com>
2021-07-29 11:14:21 +0800

    feat: add jsx plugin

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -1,8 +1,11 @@
+// yarn dev 用到的vite配置
+
 /**
  * @type {import('vite').UserConfig}
  */
-export default {
-  optimizeDeps: {
-    include: [],
-  },
-};
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [vueJsx()],
+});
